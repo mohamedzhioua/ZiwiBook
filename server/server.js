3e36045b7cd5dc12813751b39dcffee915f0e457
mohamedzhioua <zhiouamohamed1989@gmail.com>
2022-11-28 16:31:56 +0100

    added express server to the backend

---
+++ server/server.js
@@ -0,0 +1,21 @@
+// Import dependencies
+const express = require("express");
+
+//load env variables
+require("dotenv").config();
+const app = express()
+const PORT = process.env.PORT || 5000;
+
+// express app config
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/../client/public"));
+
+
+
+
+
+app.listen(PORT, function () {
+    console.log(`Server Runs Perfectly at http://localhost:${PORT}`);
+  });
