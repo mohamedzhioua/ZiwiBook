701a9c3716619afb683de747978f8c79b5c4a5d8
mohamedzhioua <zhiouamohamed1989@gmail.com>
2022-11-30 18:41:32 +0100

    added update method for post controller

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -4,20 +4,34 @@ const Post = require("../models/post");
 const PostValidation = require("../validator/PostValidation");
 module.exports = {
   addPost: async (req, res) => {
-    const { errors, isvalid } = PostValidation(req.body);
+    const { errors, isValid } = PostValidation(req.body);
     try {
-      if (!isvalid) {
+      if (!isValid) {
         res.status(404).json(errors);
       } else {
         await Post.create(req.body);
-        res.status(201).json({message:"post added with sucess"})
+        res.status(201).json({ message: "post added with success" });
       }
     } catch (error) {
-        console.log(error.message);
+      console.log(error.message);
     }
   },
-  updatePost: async (req, res) => {},
-  deletePost: async (req, res) => {},
+  updatePost: async (req, res) => {
+    const { errors, isValid } = PostValidation(req.body);
+    try {
+      if (!isValid) {
+        res.status(404).json(errors);
+      } else {
+        await Post.findByIdAndUpdate({ _id: req.params.id });
+        res.status(201).json({ message: "post updated with success" });
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  },
+  deletePost: async (req, res) => {
+
+  },
   getOnePost: async (req, res) => {},
   getAllPost: async (req, res) => {},
 };
