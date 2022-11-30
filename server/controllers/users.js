cd5611d85a4c509dd4352da3fec55d46861a24c8
mohamedzhioua <zhiouamohamed1989@gmail.com>
2022-11-30 13:39:49 +0100

    installing jwt and bcryptjs dependencies

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,9 +1,10 @@
+const User = require("../models/user");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");module.exports = {
+  //  --------------------------------------- //signup method to add a new user//--------------------------- //
+  signup: async (req, res) => {
 
-const User = require('../models/user')
-
-module.exports = {
-    signup :   async (req, res) => {},
-    signin :   async (req, res) => {},
-    logout :   async (req, res) => {},
-  
-}
+  },
+  signin: async (req, res) => {},
+  logout: async (req, res) => {},
+};
