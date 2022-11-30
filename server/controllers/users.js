48b252e339ef0ee48578e11a94e3e39fab58c718
mohamedzhioua <zhiouamohamed1989@gmail.com>
2022-11-30 12:43:30 +0100

    adding the user controller skeleton

---
+++ server/controllers/users.js
@@ -0,0 +1,9 @@
+
+const User = require('../models/user')
+
+module.exports = {
+    signup :   async (req, res) => {},
+    signin :   async (req, res) => {},
+    logout :   async (req, res) => {},
+  
+}
