ae3595dbf696316d19287f194890b58bb191764c
mohamedzhioua <zhiouamohamed1989@gmail.com>
2022-11-30 13:18:34 +0100

    adding a signin input validator in the validator folder

---
+++ server/validator/SigninValidation.js
@@ -0,0 +1,26 @@
+const validator = require("validator");
+const isEmpty = require("./IsEmpty");
+
+module.exports = function SigninValidation(data) {
+  let errors = {};
+
+  // Convert empty fields to an empty string so we can use validator
+  data.email = !isEmpty(data.email) ? data.email : "";
+  data.password = !isEmpty(data.password) ? data.password : "";
+
+  // Email checks
+  if (validator.isEmpty(data.email)) {
+    errors.email = "Email field is required";
+  } else if (!validator.isEmail(data.email)) {
+    errors.email = "Format Email required";
+  }
+
+  // Password checks
+  if (validator.isEmpty(data.password)) {
+    errors.password = "Password field is required";
+  }
+  return {
+    errors,
+    isValid: isEmpty(errors),
+  };
+};
