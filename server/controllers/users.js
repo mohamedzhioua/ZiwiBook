5042bd51e7f5cf398aa611c0c522130b40219847
mohamedzhioua <zhiouamohamed1989@gmail.com>
2022-11-30 13:47:10 +0100

    added signup method for user controller

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,9 +1,38 @@
-const User = require("../models/user");
 const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");module.exports = {
+const jwt = require("jsonwebtoken");
+// Load User model
+const User = require("../models/user");
+// Load input validation
+const SignupValidation =require('../validator/SignupValidation')
+module.exports = {
   //  --------------------------------------- //signup method to add a new user//--------------------------- //
   signup: async (req, res) => {
+    const { firstname, lastname, email, password } = req.body;
+    const { errors, isValid } = SignupValidation(req.body);
 
+    try {
+      if (!isValid) {
+        res.status(404).json(errors);
+      } else {
+        await User.findOne({ email }).then(async (exist) => {
+          if (exist) {
+            errors.email = "Email already in use";
+            res.status(404).json(errors);
+          } else {
+            const hashedpassword = bcrypt.hashSync(password, 8);
+            await User.create({
+              firstname,
+              lastname,
+              email,
+              password: hashedpassword,
+            });
+            res.status(201).json({ message: "user added with success" });
+          }
+        });
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
   },
   signin: async (req, res) => {},
   logout: async (req, res) => {},
