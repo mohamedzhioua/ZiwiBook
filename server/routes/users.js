fb735026f3e7c782c865de922658e982a967d83a
mohamedzhioua <zhiouamohamed1989@gmail.com>
2022-11-30 12:46:24 +0100

    adding user routes

---
+++ server/routes/users.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const UserController = require("../controllers/users")
+
+
+// POST request for creating a new user.
+router.post("/signup", UserController.signup);
+
+// GET request for user login.
+router.post("/signin", UserController.signin);
