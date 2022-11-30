231c89d3250b087b082f7da96a2fee29f7a770d9
mohamedzhioua <zhiouamohamed1989@gmail.com>
2022-11-30 13:35:04 +0100

    added a input validation for the blog post

---
+++ server/validator/PostValidation.js
@@ -0,0 +1,20 @@
+const validator = require("validator");
+const isEmpty = require("./IsEmpty");
+
+module.exports = function PostValidation(data) {
+  let errors = {};
+  data.title = !isEmpty(data.title) ? data.title : "";
+  data.body = !isEmpty(data.body) ? data.body : "";
+
+  if (validator.isEmpty(data.title)) {
+    errors.title = "Required title";
+  }
+  if (validator.isEmpty(data.body)) {
+    errors.body = "Required blog content";
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors),
+  };
+};
--- server/validator/SignupValidation.js
+++ server/validator/SignupValidation.js
@@ -5,12 +5,17 @@ module.exports = function SignupValidation(data) {
   let regex = /(?=(.*[0-9]))((?=.*[A-Za-z0-9])(?=.*[A-Z])(?=.*[a-z]))^.{8,}$/i;
   let errors = {};
   // Convert empty fields to an empty string so we can use validator
-  data.name = !isEmpty(data.name) ? data.name : "";
+  data.firstname = !isEmpty(data.firstname) ? data.firstname : "";
+  data.lastname = !isEmpty(data.lastname) ? data.lastname : "";
   data.email = !isEmpty(data.email) ? data.email : "";
   data.password = !isEmpty(data.password) ? data.password : "";
-  // Name checks
-  if (validator.isEmpty(data.name)) {
-    errors.name = "Name field is required";
+
+  // firstname checks
+  if (validator.isEmpty(data.firstname)) {
+    errors.firstname = "firstname field is required";
+  } // lastname checks
+  if (validator.isEmpty(data.lastname)) {
+    errors.lastname = "lastname field is required";
   }
   // Email checks
   if (validator.isEmpty(data.email)) {
