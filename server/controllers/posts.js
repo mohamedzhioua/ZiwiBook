b8b60f68527327118348da29164b74eb62bfec68
mohamedzhioua <zhiouamohamed1989@gmail.com>
2022-11-30 18:35:27 +0100

    added addPost method to the posts controller

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,9 +1,23 @@
-const Post = require('../models/post')
-
+// Load Post model
+const Post = require("../models/post");
+// Load input validation
+const PostValidation = require("../validator/PostValidation");
 module.exports = {
-    addPost :   async (req, res) => {},
-    updatePost :   async (req, res) => {},
-    deletePost :   async (req, res) => {},
-    getOnePost :   async (req, res) => {},
-    getAllPost :   async (req, res) => {}
-}
+  addPost: async (req, res) => {
+    const { errors, isvalid } = PostValidation(req.body);
+    try {
+      if (!isvalid) {
+        res.status(404).json(errors);
+      } else {
+        await Post.create(req.body);
+        res.status(201).json({message:"post added with sucess"})
+      }
+    } catch (error) {
+        console.log(error.message);
+    }
+  },
+  updatePost: async (req, res) => {},
+  deletePost: async (req, res) => {},
+  getOnePost: async (req, res) => {},
+  getAllPost: async (req, res) => {},
+};
