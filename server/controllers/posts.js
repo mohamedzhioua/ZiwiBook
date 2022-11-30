f88b8465e9a612b875f2becdb9e3b30e416fb057
mohamedzhioua <zhiouamohamed1989@gmail.com>
2022-11-30 12:41:56 +0100

    adding the post controller skeleton

---
+++ server/controllers/posts.js
@@ -0,0 +1,9 @@
+const Post = require('../models/post')
+
+module.exports = {
+    addPost :   async (req, res) => {},
+    updatePost :   async (req, res) => {},
+    deletePost :   async (req, res) => {},
+    getOnePost :   async (req, res) => {},
+    getAllPost :   async (req, res) => {}
+}
