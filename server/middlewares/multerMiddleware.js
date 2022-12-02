5d70d6b5eca37e8ce58f2006dbe19e3316b2e100
mohamedzhioua <zhiouamohamed1989@gmail.com>
2022-12-02 12:00:43 +0100

    adding multer middleware method

---
+++ server/middlewares/multerMiddleware.js
@@ -0,0 +1,23 @@
+const multer = require('multer');
+
+const storage = multer.memoryStorage()
+
+module.exports = multer(
+    {
+        storage: storage,
+        //limiting file size by 5Mb
+        limits: { fileSize: 5 * 1024 * 1024 },
+        //accepting only jpg jpeg png files
+        fileFilter: function (req, file, cb) {
+            const fileRegex = new RegExp('\.(jpg|jpeg|png)$');
+            const fileName = file.originalname;
+
+            if (!fileName.match(fileRegex)) {
+                //throw exception
+                return cb(new Error('Invalid file type'));
+            }
+            //pass the file
+            cb(null, true);
+        }
+    })
+    .single('image'); //single for accepting only one file from 'image' form-data key
