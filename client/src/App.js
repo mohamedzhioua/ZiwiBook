c8a549ebd05689ca0c7a5d77586dc03ddb9ff7c6
mohamedzhioua <zhiouamohamed1989@gmail.com>
2022-12-19 12:22:29 +0100

    installing mdbootstrap and fixing his autoprefixer warning

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter } from "react-router-dom";
 import Router from "./routes/Router";
 import Header from "./layouts/Header/Header";
 import { Footer } from "./layouts/Footer/Footer";
-
+ 
 function App() {
   return (
     <BrowserRouter>
--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
