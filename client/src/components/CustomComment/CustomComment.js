cbb98b5a90a4323a8380cb933a53475693cff32b
mohamedzhioua <zhiouamohamed1989@gmail.com>
2023-01-10 18:58:11 +0100

    styling the CustomComment component

--- client/src/components/CustomComment/CustomComment.js
+++ client/src/components/CustomComment/CustomComment.js
@@ -1,22 +1,33 @@
 import React, { useState } from "react";
 import CustomInput from "../CustomInput/CustomInput";
+import CustomButton from "../CustomButton/CustomButton";
 import "./CustomComment.css";
-const CustomComment = () => {
+const CustomComment = ({ post }) => {
   const [comments, setComments] = useState([1, 2, 3, 4]);
+  const [comment, setComment] = useState("");
+
   return (
     <div className="container">
       <div className="container">
         {comments.map((comment, index) => (
-          <div>
-            <p className="comment-p" key={index}>
-              comment{index}
-            </p>
+          <div key={index}>
+            <p className="comment-p">comment{index}</p>
             <hr />
           </div>
         ))}
       </div>
       <div>
-        <CustomInput type="textarea" float label="write a Comment" plaCeholder="write a Comment"/>
+        <div>
+        <CustomInput
+          type="textarea"
+          float
+          label="write a Comment . . . ."
+          placeholder="write a Comment . . . ."
+          value={comment}
+          onChange={(e) => setComment(e.target.value)}
+        />
+        <CustomButton value="comment" className="commentbtn"/>
+        </div>
       </div>
     </div>
   );
