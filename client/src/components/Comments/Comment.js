7e5742b70711a7e1f30884847a242b9a6aa8c1fd
mohamedzhioua <zhiouamohamed1989@gmail.com>
2023-01-15 14:50:11 +0100

    adding comment actions delete/reply/edit to the body of comments as a architucture to follow

--- client/src/components/Comments/Comment.js
+++ client/src/components/Comments/Comment.js
@@ -1,23 +1,37 @@
 import React from "react";
+import { useDispatch } from "react-redux";
 import moment from "moment";
 // Styles
 import "./index.css";
 
-const Comment = ({ comment, replies }) => {
+const Comment = ({ comment, replies, CurrentUserId }) => {
+  const dispatch = useDispatch();
+  const canReply = Boolean(CurrentUserId);
+  const canEdit = CurrentUserId === comment?.owner?._id;
+  const canDelete = CurrentUserId === comment?.owner?._id;
+  
   return (
     <div className="comment">
       <img className="comment-image" src={comment?.owner?.image} alt="." />
       <div className="comment-body">
-        <div className="info">
+        <div className="comment-info">
           <span className="comment-author">{comment?.owner?.name}</span>
           <span className="date">{moment(comment?.createdAt).fromNow()}</span>
         </div>
         <p className="comment-text">{comment?.text}</p>
-        <div className="replies">
-          {replies.map((reply, i) => (
-            <Comment comment={reply} key={i} replies={[]} />
-          ))}
+        <div className="comment-actions">
+          {canReply && <div className="comment-action"onClick={dispatch()}>Reply</div>}
+          {canEdit && <div className="comment-action" onClick={dispatch()}>Edit</div>}
+          {canDelete && <div className="comment-action" onClick={dispatch()}>Delete</div>}
         </div>
+
+        {replies.length > 0 && (
+          <div className="replies">
+            {replies.map((reply, i) => (
+              <Comment comment={reply} key={i} replies={[]} CurrentUserId={CurrentUserId}/>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
--- client/src/components/Comments/index.js
+++ client/src/components/Comments/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 // features
-import { AddComment} from "../../app/features/memorie/postSlice";
+import { AddComment } from "../../app/features/memorie/postSlice";
 
 // Components
 import Comment from "./Comment";
@@ -12,37 +12,60 @@ import CommentForm from "./CommentForm";
 import "./index.css";
 
 const Comments = ({ post }) => {
-  const {comments} = useSelector((state) =>state.post);
-const rootComments = comments.filter((comment) => comment.post === post?._id).sort((a,b)=>b.createdAt.localeCompare(a.createdAt))
-// const getReplies = commentID =>{
-//   return comments.filter((comment)=>comment._id === commentID).sort((a,b)=>b.replies.createdAt.localeCompare(a.replies.createdAt))
-// }
-   const dispatch = useDispatch();
+  const CurrentUserId = useSelector((state) => state.auth.user._id);
+  const { comments } = useSelector((state) => state.post);
+  const rootComments = comments
+    .filter((comment) => comment.post === post?._id)
+    .sort((a, b) => b.createdAt.localeCompare(a.createdAt));
+  // const getReplies = commentID =>{
+  //   return comments.filter((comment)=>comment._id === commentID).sort((a,b)=>b.replies.createdAt.localeCompare(a.replies.createdAt))
+  // }
+  const dispatch = useDispatch();
   const id = post?._id;
 
-
-
   //onsubmitHandler
-  const addComment = (text ) => {
-    dispatch(AddComment({id,text}))
-   console.log("🚀 ~ file: index.js:28 ~ addComment ~ text", id , text)
+  const addComment = (text) => {
+    dispatch(AddComment({ id, text }));
   };
 
   return (
     <>
       <div className="comments">
         <div className="write">
-        <img className="comments-img"
+          <img
+            className="comments-img"
             src="https://thumbs.dreamstime.com/b/businessman-icon-vector-male-avatar-profile-image-profile-businessman-icon-vector-male-avatar-profile-image-182095609.jpg"
             alt="."
           />
-          <CommentForm  submitLabel="write" handleSubmit={addComment}/>
-         </div>
+          <CommentForm submitLabel="write" handleSubmit={addComment} />
+        </div>
         <div className="comments-container">
-        {rootComments?.map((rootComment) => (
-         <Comment key={rootComment._id} comment={rootComment} replies={[{text:"rrrrrrrrrrrrrrr" , owner:{name:"hama hama",image:"https://thumbs.dreamstime.com/b/businessman-icon-vector-male-avatar-profile-image-profile-businessman-icon-vector-male-avatar-profile-image-182095609.jpg"}},{text:"bbbbbbbbbbbb" , owner:{name:"ziwi ziwi",image:"https://thumbs.dreamstime.com/b/businessman-icon-vector-male-avatar-profile-image-profile-businessman-icon-vector-male-avatar-profile-image-182095609.jpg"}}]}/>
-        ))}
-      </div>
+          {rootComments?.map((rootComment) => (
+            <Comment
+              key={rootComment._id}
+              comment={rootComment}
+              CurrentUserId={CurrentUserId}
+              replies={[
+                {
+                  text: "rrrrrrrrrrrrrrr",
+                  owner: {
+                    name: "hama hama",
+                    image:
+                      "https://thumbs.dreamstime.com/b/businessman-icon-vector-male-avatar-profile-image-profile-businessman-icon-vector-male-avatar-profile-image-182095609.jpg",
+                  },
+                },
+                {
+                  text: "bbbbbbbbbbbb",
+                  owner: {
+                    name: "ziwi ziwi",
+                    image:
+                      "https://thumbs.dreamstime.com/b/businessman-icon-vector-male-avatar-profile-image-profile-businessman-icon-vector-male-avatar-profile-image-182095609.jpg",
+                  },
+                },
+              ]}
+            />
+          ))}
+        </div>
       </div>
     </>
   );
