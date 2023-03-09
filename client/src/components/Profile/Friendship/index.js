2d9c788786b89214004ff8984dcc27d5796cf384
mohamedzhioua <zhiouamohamed1989@gmail.com>
2023-03-09 19:35:59 +0100

    accepting friends request run perfectly

--- client/src/components/Profile/Friendship/index.js
+++ client/src/components/Profile/Friendship/index.js
@@ -1,19 +1,26 @@
 import { useRef, useState } from "react";
 import { useFriendFuncMutation } from "../../../app/features/friend/friendSlice";
+import useOnClickOutside from "../../../hooks/useOnClickOutside";
 import { Card, CustomButton } from "../../index";
 import style from "./Friendship.module.css";
 
 function Friendship({ userId ,userfriendshipdata}) {
   const menu = useRef(null);
+  const menuRef = useRef(null);
   const [respondMenu, setRespondMenu] = useState(false);
+  const [friendsMenu, setFriendsMenu] = useState(false);
 
   const [FriendFunc] = useFriendFuncMutation();
+  useOnClickOutside(menu, respondMenu, () => {
+    setRespondMenu(false);
+  });
 
   const addFriendHandler = () => {
     FriendFunc({ id: userId , type: "add"});
   };
   const acceptRequestHanlder = () => {
-    FriendFunc ({ id: userfriendshipdata.requestID, type: "accept" });
+    FriendFunc({ id: userfriendshipdata.requestID, type: "accept" });
+    setRespondMenu(false);
   };
 
   const cancelRequestHandler = () => {
@@ -22,14 +29,34 @@ function Friendship({ userId ,userfriendshipdata}) {
 
   return (
     <div className={style.container}>
-      
-     { !userfriendshipdata?.requestSent &&
+      {userfriendshipdata?.friends ? (
+        <>
+          <CustomButton
+          value="Friends"
+          className="gray_btn btns"
+            onClick={() => setFriendsMenu((perv) => !perv)}
+          >
+          </CustomButton>
+          {friendsMenu && (
+            <Card className={style.open_menu} innerRef={menuRef}>
+              <div
+                className={`${style.item} hover1`}
+              >
+                Unfriend
+              </div>
+            </Card>
+          )}
+        </>
+      ):( 
+     !userfriendshipdata?.requestSent &&
      !userfriendshipdata?.requestReceived &&
       (<CustomButton
         value="Add as A friend"
         className={`blue_btn btns`}
         onClick={() => addFriendHandler()}
-      />)}
+      />
+      )
+      )}
         {userfriendshipdata?.requestSent ? (
         <CustomButton className="blue_btn btns"
         value="Cancel Request"
