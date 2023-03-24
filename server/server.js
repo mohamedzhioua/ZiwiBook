b15ea373733b13f2045064383cb134c5db45e195
mohamedzhioua <zhiouamohamed1989@gmail.com>
2023-03-24 13:38:47 +0100

    adding 404html file to handle the requests to not exixting routes

--- server/server.js
+++ server/server.js
@@ -1,13 +1,14 @@
-// Import dependencies
+const path = require('path')
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const { createServer } = require("http");
+const connectDB = require("./config/db");
 const corsOptions = require ("./utils/corsOptions")
 const allowedOrigins = require("./utils/allowedOrigins")
 const app = express();
 
-
+// connectDB()
 app.use(cors((corsOptions)));
 
 //limiter for posts 
@@ -25,7 +26,6 @@ app.use(cors((corsOptions)));
 // app.use('/api/posts/addPost', limiter);
 
 // database
-const db = require("./config/db");
 //load env variables
 require("dotenv").config();
 const PORT = process.env.PORT || 3070;
@@ -37,15 +37,25 @@ app.use(express.json({ limit: "5000kb" })); // LIMIT for JSON
 app.use(express.urlencoded({ extended: true, limit: "5000kb" })); // LIMIT for URL ENCODE (image data)
 app.use(cookieParser());
 
-//Require application Route modules
-const userRoutes = require("./routes/users");
-const postRoutes = require("./routes/posts");
-const friendRoutes = require("./routes/friends");
-const NotificationRoutes = require("./routes/notifications");
-app.use("/api/users", userRoutes);
-app.use("/api/posts", postRoutes);
-app.use("/api/friends", friendRoutes);
-app.use("/api/notifications", NotificationRoutes);
+app.use('/', express.static(path.join(__dirname, 'public')))
+
+//Routes
+app.use("/api/users", require("./routes/users"));
+app.use("/api/posts",  require("./routes/posts"));
+app.use("/api/friends", require("./routes/friends"));
+app.use("/api/notifications", require("./routes/notifications"));
+
+app.all('*', (req, res) => {
+  res.status(404)
+  if (req.accepts('html')) {
+      res.sendFile(path.join(__dirname, 'views', '404.html'))
+  } else if (req.accepts('json')) {
+      res.json({ message: '404 Not Found' })
+  } else {
+      res.type('txt').send('404 Not Found')
+  }
+})
+
 
 const httpServer = createServer(app);
 const sio = require("./utils/socket");
