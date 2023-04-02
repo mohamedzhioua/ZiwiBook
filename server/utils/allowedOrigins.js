168c5cf038afd40911217d83cf68c5172388400f
mohamedzhioua <zhiouamohamed1989@gmail.com>
2023-04-02 16:23:32 +0100

    fixing cors error

--- server/utils/allowedOrigins.js
+++ server/utils/allowedOrigins.js
@@ -1,3 +1,3 @@
-const allowedOrigins = ["http://127.0.0.1:5173", "http://localhost:3000","https://ziwiibook.vercel.app","https://ziwiibook-p6mi2d04e-zhiouamohamed1989-gmailcom.vercel.app/login" ];
+const allowedOrigins = ["http://127.0.0.1:5173", "http://localhost:3000","https://ziwiibook.vercel.app"," https://ziwiibook-6em3zhaxq-zhiouamohamed1989-gmailcom.vercel.app" ];
 
 module.exports = allowedOrigins
