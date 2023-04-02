3c51f707266d4f21811c8adc8954151ba00d0518
mohamedzhioua <zhiouamohamed1989@gmail.com>
2023-04-02 15:43:27 +0100

    preparing for deploy

--- client/src/app/api/apiSlice.jsx
+++ client/src/app/api/apiSlice.jsx
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: 'api', // optional
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000" ,
+    baseUrl: import.meta.env.VITE_API_BASE_URL ,
     credentials: "include",
   }),
   tagTypes: ["Post", "Comment", "Reaction", "Photo","Userprofile", "Notif"],
